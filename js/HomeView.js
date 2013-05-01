db17791a29def2de7035fe695a5ebaa50d41d705
DzulAiman <dzul.aiman@gmail.com>
2013-05-01 19:21:18 +0800

    debug alert

--- js/HomeView.js
+++ js/HomeView.js
@@ -1,31 +1,10 @@
 var HomeView = function(store) {
 
-	//renderHomeView: function() {
-	//	$('body').html(this.homeTpl());
-	//	$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	//},
 
 	this.render = function() {
 		this.el.html(HomeView.template());
 		return this;
 	};
-	
-	//findByName: function() {
-        //console.log('findByName');
-        //this.store.findByName($('.search-key').val(), function(employees) {
-        //    var l = employees.length;
-        //    var e;
-        //    $('.employee-list').empty();
-        //    for (var i=0; i<l; i++) {
-        //        e = employees[i];
-        //       $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-        //    }
-        //});
-		//var self = this;
-		//this.store.findByName($('.search-key').val(),function(employees){
-		//	$('.employee-list').html(self.employeeLiTpl(employees));
-		//});
-    //},
 	
 	this.findByName = function() {
 		store.findByName($('.search-key').val(),function(employees){
